708e5ccaaa5a5ad7e9db515947014e8f1594afa5
Alex Fabiano <afabiano15@gmail.com>
2017-08-13 13:42:24 -0400

    Adicionando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from "app/hero.service";
 
 @NgModule({
   declarations: [
@@ -16,9 +18,17 @@ import { HeroesComponent } from './heroes/heroes.component';
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,7 +5,6 @@ import { HeroService } from "app/hero.service";
 @Component({
   selector: 'my-heroes',
   template: `
-    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
@@ -64,8 +63,7 @@ import { HeroService } from "app/hero.service";
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `],
-  providers: [HeroService]
+  `]
 })
 export class HeroesComponent implements OnInit {
   
