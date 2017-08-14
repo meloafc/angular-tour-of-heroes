f64d472abf2411f4a44c38bf05655b1fb8c3a214
Alex Fabiano <afabiano15@gmail.com>
2017-08-13 20:36:55 -0400

    Adicionando heróis no dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from "app/hero";
+import { HeroService } from "app/hero.service";
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
 
-  ngOnInit() {
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1,5));
   }
 
 }
