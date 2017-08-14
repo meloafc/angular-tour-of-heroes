19ced7a5134a18d70d92d58ba25a297455778686
Alex Fabiano <afabiano15@gmail.com>
2017-08-13 20:27:12 -0400

    Adicionando um dashboard

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-dashboard',
+  template: '<h3>My Dashboard</h3>'
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
